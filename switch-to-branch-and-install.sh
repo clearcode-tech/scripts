aeff5a67dd2e6812ce158a323a0e98380a3391c3
Pashinsky Mikhail <mpashinsky@gmail.com>
2022-12-23 13:56:41 +0300

    master Исправлен вызов git fetch

--- switch-to-branch-and-install.sh
+++ switch-to-branch-and-install.sh
@@ -5,10 +5,10 @@ branchName=$1
 dirPrefix="candidate"
 dirs=("commons" "api-gateway" "auth" "candidate" "file" "id" "invitation" "notification" "request-logger" "session" "tenant" "websocket")
 
-git fetch --all
 for dir in "${dirs[@]}"
 do
   cd $(echo $dirPrefix-$dir)
+  git fetch --all
   git show-branch remotes/origin/"$branchName" &> /dev/null
   if [ $? -eq 0 ]; then
     git checkout "$branchName"
